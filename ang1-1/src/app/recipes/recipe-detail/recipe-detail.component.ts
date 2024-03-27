ef2e52fd1d1f94bce7894b99629d4ec4f4d14c96
Omaru Calla Kamara <student7.ubuntuafrika@gmail.com>
2024-03-27 12:57:27 +0000

    Recipe Book new commit

---
+++ ang1-1/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrl: './recipe-detail.component.css'
+})
+export class RecipeDetailComponent implements OnInit {
+  @Input() recipe: Recipe;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
